ab4869ee375890071309d48ff708c4617328b8d0
Pedro Piveta Barrotti <102613650+PedroPiveta@users.noreply.github.com>
2022-06-22 18:52:30 +0000

    jogo quase completo

--- code/main.js
+++ code/main.js
@@ -4,20 +4,124 @@ import kaboom from "kaboom"
 kaboom()
 
 // load assets
-loadSprite("bean", "sprites/bean.png")
+loadSprite("birdy", "sprites/turtle_king-removebg-preview (1).png");
 
-// add a character to screen
+loadSprite("bg", "sprites/flappy-bg.png");
+loadSprite("cano-cima", "sprites/cano-removebg-preview.png");
+loadSound("wooosh", "sounds/wooosh.mp3");
+loadSound("score", "sounds/score.mp3");
+
+let highScore = 0;
+scene("inicio", () => {
+  add([
+   sprite("bg", {width:width(), height: height()})
+  ]);
+  
+  add([
+    text("Flappy Turtle King \nPressione a barra\nde espaco para jogar", {size:30})
+  ]);
+  keyPress("space", () => {
+  go("game")
+  });
+})
+scene("game", () => {
+  
+const PIPE_GAP = 150;
+
+let score = 0;
+
 add([
+  sprite("bg", {width:width(), height: height()})
+]);
+
+const scoreText = add([
+  text(score, {size: 50})
+]);
+  
+const player = add([
 	// list of components
-	sprite("bean"),
+	sprite("birdy"),
+  scale(2),
 	pos(80, 40),
 	area(),
-])
+  body(),
+]);
 
-// add a kaboom on mouse click
-onClick(() => {
-	addKaboom(mousePos())
-})
 
-// burp on "b"
-onKeyPress("b", burp)
+
+function producePipes(){
+const offset = rand(-50, 50)
+  
+add([
+  sprite('cano-cima'),
+  scale(0.25),
+  pos(width(), height()/2 + offset + PIPE_GAP/2),
+  "pipe",
+  area(),
+  {passed: false}
+]);
+
+add([
+  sprite('cano-cima', {flipY: true}),
+  scale(0.25),
+  pos(width(), height()/2 + offset - PIPE_GAP/2),
+  origin("botleft"),
+  "pipe",
+  area()
+]);
+}
+
+loop(1.5, () => {
+  producePipes();
+});
+
+
+  
+action("pipe", (pipe) => {
+  pipe.move(-160,0);
+
+  if (pipe.passed === false && pipe.pos.x < player.pos.x){
+    pipe.passed = true;
+    play("score");
+    score+=1;
+    scoreText.text = score;
+    //debug.log(score);
+  }
+});
+
+player.collides("pipe", () => {
+  go("gameover", score);
+});
+
+player.action(() => {
+  if (player.pos.y > height() +30 || player.pos.y < -30){
+    go("gameover", score);
+  }
+});
+  
+keyPress("space", () => {
+  play("wooosh");
+  player.jump(400);
+  });
+});
+
+scene("gameover", (score) => {
+  
+  add([
+  sprite("bg", {width:width(), height: height()})
+]);
+  
+  if(score > highScore){
+    highScore = score;
+  }
+  
+  add([
+    text("Fim de jogo!\n" + "Pontos: " + score + "\nRecorde: " + highScore)
+  ]);
+
+  keyPress("space", () => {
+    go("game");
+  })
+});
+
+go("inicio");
--- dist/game.js
+++ dist/game.js
@@ -2914,15 +2914,98 @@ vec4 frag(vec3 pos, vec2 uv, vec4 color, sampler2D tex) {
 
   // code/main.js
   no();
-  loadSprite("bean", "sprites/bean.png");
-  add([
-    sprite("bean"),
-    pos(80, 40),
-    area()
-  ]);
-  onClick(() => {
-    addKaboom(mousePos());
+  loadSprite("birdy", "sprites/turtle_king-removebg-preview (1).png");
+  loadSprite("bg", "sprites/flappy-bg.png");
+  loadSprite("cano-cima", "sprites/cano-removebg-preview.png");
+  loadSound("wooosh", "sounds/wooosh.mp3");
+  loadSound("score", "sounds/score.mp3");
+  var highScore = 0;
+  scene("inicio", () => {
+    add([
+      sprite("bg", { width: width(), height: height() })
+    ]);
+    add([
+      text("Flappy Turtle King \nPressione a barra\nde espaco para jogar", { size: 30 })
+    ]);
+    keyPress("space", () => {
+      go("game");
+    });
   });
-  onKeyPress("b", burp);
+  scene("game", () => {
+    const PIPE_GAP = 150;
+    let score = 0;
+    add([
+      sprite("bg", { width: width(), height: height() })
+    ]);
+    const scoreText = add([
+      text(score, { size: 50 })
+    ]);
+    const player = add([
+      sprite("birdy"),
+      scale(2),
+      pos(80, 40),
+      area(),
+      body()
+    ]);
+    function producePipes() {
+      const offset = rand(-50, 50);
+      add([
+        sprite("cano-cima"),
+        scale(0.25),
+        pos(width(), height() / 2 + offset + PIPE_GAP / 2),
+        "pipe",
+        area(),
+        { passed: false }
+      ]);
+      add([
+        sprite("cano-cima", { flipY: true }),
+        scale(0.25),
+        pos(width(), height() / 2 + offset - PIPE_GAP / 2),
+        origin("botleft"),
+        "pipe",
+        area()
+      ]);
+    }
+    __name(producePipes, "producePipes");
+    loop(1.5, () => {
+      producePipes();
+    });
+    action("pipe", (pipe) => {
+      pipe.move(-160, 0);
+      if (pipe.passed === false && pipe.pos.x < player.pos.x) {
+        pipe.passed = true;
+        play("score");
+        score += 1;
+        scoreText.text = score;
+      }
+    });
+    player.collides("pipe", () => {
+      go("gameover", score);
+    });
+    player.action(() => {
+      if (player.pos.y > height() + 30 || player.pos.y < -30) {
+        go("gameover", score);
+      }
+    });
+    keyPress("space", () => {
+      play("wooosh");
+      player.jump(400);
+    });
+  });
+  scene("gameover", (score) => {
+    add([
+      sprite("bg", { width: width(), height: height() })
+    ]);
+    if (score > highScore) {
+      highScore = score;
+    }
+    add([
+      text("Fim de jogo!\nPontos: " + score + "\nRecorde: " + highScore)
+    ]);
+    keyPress("space", () => {
+      go("game");
+    });
+  });
+  go("inicio");
 })();
 //# sourceMappingURL=game.js.map
